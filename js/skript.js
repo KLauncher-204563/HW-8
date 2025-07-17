6563f962a101f7f02caa35597abeef84d62cc129
Ivan <Lugovou_Ivan@ukr.net>
2025-07-17 13:27:56 +0300

    Js

---
+++ js/skript.js
@@ -0,0 +1,29 @@
+let age = 15;
+console.log(age);
+
+let name = "Іван";
+console.log(name);
+
+let isStudent = true; 
+console.log(isStudent);
+
+let myString = "Ніколи не здавайся, навіть коли важко!";
+console.log(myString);
+
+let myNumber = 15;
+myNumber = myNumber + 10; 
+console.log(myNumber); 
+
+let myNull = null;
+console.log(myNull);
+
+let userName = prompt("Введіть своє ім’я");
+alert("Ваше ім’я: " + userName);
+console.log(typeof userName); 
+
+let userAnswer = confirm("Ви дійсно хочете покинути сторінку?");
+console.log(userAnswer); 
+
+alert("Увага! Дія може бути небезпечною.");
+let confirmDanger = confirm("Ви впевнені, що хочете продовжити?");
+console.log("Користувач підтвердив дію:", confirmDanger);
